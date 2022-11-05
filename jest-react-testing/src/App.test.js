0ef2ccd595ce4a423afda1603086a3d9582ca9ae
Susan Heller <*jefa8@gmail.com>
2022-11-05 11:50:32 -0400

    need to do question 5

--- jest-react-testing/src/App.test.js
+++ jest-react-testing/src/App.test.js
@@ -1,8 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+//below is a commented-out example of test systax
+// test("your test description", () => {})
+
+test("reader renders with correct text", () => {
+  render(<App />)
+  const headerE1 = screen.getByRole("heading")
+  expect (headerE1.textContent) .toBe("Testing Playground")
+})
+
+
+describe('test for the button', () => {
+test("button changes color when clicked", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+
+  userEvent.click(colorBtn)
+
+  expect(colorBtn) .toHaveStyle({ backgroundColor: "blue"})
+  expect(colorBtn.textContent) .toBe("Change button color to green")
+})
+})
+
+test("checkbox disables button when first clicked, then enables button on second click", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole("button")
+  const checkbox = screen.getByRole("checkbox")
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeDisabled()
+
+  userEvent.click(checkbox)
+  expect(colorBtn).toBeEnabled()
+})
+
+test("", () => {
+  render(<App />)
+  
+})
